72a0b07c9f8e92d58fd194d7fd30e885e8d80c90
mongkyo <dreamong91@gmail.com>
2019-01-07 12:24:35 +0900

    [DB] migrate members.User

--- app/members/models.py
+++ app/members/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    img_profile = models.ImageField(
+        '프로필 이미지',
+        upload_to='user',
+        blank=True,
+        null=True,
+    )
