b1241c822a02f298915920304a1d81d02c572658
mongkyo <dreamong91@gmail.com>
2019-01-15 23:14:23 +0900

    [create] .config file

--- app/members/admin.py
+++ app/members/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import User
+
 
 # Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['id', 'username']
+    search_fields = ['username']
+    ordering = ['-id']
+
+
+admin.site.register(User, UserAdmin,)
