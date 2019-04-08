010e1640db51e2113bc3350f10af6e1155c9258e
mongkyo <dreamong91@gmail.com>
2019-04-09 01:48:00 +0900

    [modified] all of crawling without selenium in models.py

--- app/books/models.py
+++ app/books/models.py
@@ -24,49 +24,62 @@ class Book(models.Model):
         """
         책 검색 정보를 불러와준다
         """
-
-        CHROME_DIR = os.path.join(ROOT_DIR, 'crawling')
-        chromedriver_dir = os.path.join(CHROME_DIR, 'chromedriver')
-        options = webdriver.ChromeOptions()
-        options.add_argument('headless')
-        options.add_argument('window-size=1920x1080')
-        options.add_argument("disable-gpu")
-        driver = webdriver.Chrome(executable_path=chromedriver_dir, chrome_options=options)
-        driver.get('https://nsulib.nsu.ac.kr/')
-        elem = driver.find_element_by_id('type1q')
-        elem.send_keys(keyword)
-        elem.send_keys(Keys.RETURN)
-        a_tag = driver.find_elements_by_xpath('//a[contains(@href,"#previewLocation")]')
-
-        for a in a_tag[:3 + 1]:
-            time.sleep(1)
-            a.click()
-        time.sleep(1)
-        html = driver.page_source
+        res = requests.get(f'https://nsulib.nsu.ac.kr/search/tot/result?st=KWRD&si=TOTAL&q={keyword}&x=0&y=0')
+        html = res.text
         soup = BeautifulSoup(html, 'html.parser')
         ul = soup.select_one('.resultList')
         book_info = []
-        for i in range(0, 3 + 1):
+        for i in range(0, 2+1):
             li = ul.select('.items')[i]
             book_one = dict()
+            book_detail = dict()
             book_one['title'] = li.select_one('dd.title > a').get_text()
             book_one['author'] = li.select('dd.info')[0].get_text()
             book_one['publisher'] = li.select('dd.info')[1].get_text()
             book_one['publish_year'] = li.select('dd.info')[2].get_text()
             book_info.append(book_one)
-            tbody = li.select_one('tbody')
-            try:
-                for tr in tbody.find_all('tr'):
-                    book_tmp = dict()
-                    no = tr.select_one('.footable-first-column').get_text()
-                    book_tmp['no'] = tr.select_one('.footable-first-column').get_text()
-                    book_tmp['location'] = tr.select_one('.location').get_text()
-                    book_tmp['serial_num'] = tr.select_one('.callNum').get_text()
-                    book_tmp['status'] = tr.select_one('.status').get_text()
-                    book_tmp['returnDate'] = tr.select_one('.returnDate').get_text()
-                    book_one[f'{no}'] = book_tmp
-            except AttributeError:
-                pass
+            test_text = li.get('id')
+            no = re.search(r'(CAT\w+\d+)', test_text).group()
+            detail_html = requests.get(f'https://nsulib.nsu.ac.kr/search/detail/{no}').text
+            detail_soup = BeautifulSoup(detail_html, 'html.parser')
+            div_li = detail_soup.select_one('.listTable > table')
+            book_detail['no'] = div_li.select_one('td.num').get_text()
+            book_detail['callNum'] = div_li.select_one('td.callNum').get_text()
+            book_detail['location'] = div_li.select_one('td.location').get_text(" ", strip=True)
+            book_detail['status'] = div_li.select_one('span.status').get_text()
+            book_one['detail'] = book_detail
+            return book_one
+#        for a in a_tag[:3 + 1]:
+#            time.sleep(1)
+#            a.click()
+#        time.sleep(1)
+#        html = driver.page_source
+#        soup = BeautifulSoup(html, 'html.parser')
+#        ul = soup.select_one('.resultList')
+#       book_info = []
+#        for i in range(0, 3 + 1):
+ #           li = ul.select('.items')[i]
+ #           book_one = dict()
+ #           book_one['title'] = li.select_one('dd.title > a').get_text()
+  #          book_one['author'] = li.select('dd.info')[0].get_text()
+   #         book_one['publisher'] = li.select('dd.info')[1].get_text()
+    #        book_one['publish_year'] = li.select('dd.info')[2].get_text()
+#            book_info.append(book_one)
+ #           tbody = li.select_one('tbody')
+  #          try:
+   #             for tr in tbody.find_all('tr'):
+     #               book_tmp = dict()
+    #                no = tr.select_one('.footable-first-column').get_text()
+     #               book_tmp['no'] = tr.select_one('.footable-first-column').get_text()
+  #                  book_tmp['location'] = tr.select_one('.location').get_text()
+   #                 book_tmp['serial_num'] = tr.select_one('.callNum').get_text()
+            #        book_tmp['status'] = tr.select_one('.status').get_text()
+           #         book_tmp['returnDate'] = tr.select_one('.returnDate').get_text()
+          #          book_one[f'{no}'] = book_tmp
+         #   except AttributeError:
+        #        pass
 
-        driver.close()
-        return book_info
+       # driver.close()
+      #  return book_info
+
+
