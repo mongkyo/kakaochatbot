f2947cff893b8c78980060991e6846654336b16b
mongkyo <dreamong91@gmail.com>
2019-04-15 17:13:58 +0900

    [create] code refactoring in models.py

--- app/books/models.py
+++ app/books/models.py
@@ -49,38 +49,3 @@ class Book(models.Model):
             book_detail['status'] = div_li.select_one('span.status').get_text()
             book_one['detail'] = book_detail
         return book_one
-
-#        for a in a_tag[:3 + 1]:
-#            time.sleep(1)
-#            a.click()
-#        time.sleep(1)
-#        html = driver.page_source
-#        soup = BeautifulSoup(html, 'html.parser')
-#        ul = soup.select_one('.resultList')
-#       book_info = []
-#        for i in range(0, 3 + 1):
- #           li = ul.select('.items')[i]
- #           book_one = dict()
- #           book_one['title'] = li.select_one('dd.title > a').get_text()
-  #          book_one['author'] = li.select('dd.info')[0].get_text()
-   #         book_one['publisher'] = li.select('dd.info')[1].get_text()
-    #        book_one['publish_year'] = li.select('dd.info')[2].get_text()
-#            book_info.append(book_one)
- #           tbody = li.select_one('tbody')
-  #          try:
-   #             for tr in tbody.find_all('tr'):
-     #               book_tmp = dict()
-    #                no = tr.select_one('.footable-first-column').get_text()
-     #               book_tmp['no'] = tr.select_one('.footable-first-column').get_text()
-  #                  book_tmp['location'] = tr.select_one('.location').get_text()
-   #                 book_tmp['serial_num'] = tr.select_one('.callNum').get_text()
-            #        book_tmp['status'] = tr.select_one('.status').get_text()
-           #         book_tmp['returnDate'] = tr.select_one('.returnDate').get_text()
-          #          book_one[f'{no}'] = book_tmp
-         #   except AttributeError:
-        #        pass
-
-       # driver.close()
-      #  return book_info
-
-
